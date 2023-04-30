589a3a1df7b2203a071d656e2c9a45f3cd0284f0
ZeT1an <584633819@qq.com>
2023-04-30 16:46:41 +0800

    fix: v3 02-项目搭建-注册接口-登录凭证;

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -2,12 +2,11 @@ const connection = require('../app/database')
 
 class UserService {
   async create(user) {
-
     // 1.获取用户 user
-    const { name, password } = user;
+    const { name, password } = user
 
     // 2.拼接 statement
-    const statement = "INSERT INTO `user` (name, password) VALUES (?, ?);"
+    const statement = 'INSERT INTO `user` (name, password) VALUES (?, ?);'
 
     // 3.执行 sql 语句
     const [result] = await connection.execute(statement, [name, password])
@@ -16,7 +15,7 @@ class UserService {
   }
 
   async findUserByName(name) {
-    const statement = "SELECT * FROM `user` WHERE name = ?;"
+    const statement = 'SELECT * FROM `user` WHERE name = ?;'
 
     const [values] = await connection.execute(statement, [name])
 
