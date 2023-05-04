a2a1f073f4cb7316218fa33948729d9babbad596
ZeT1an <584633819@qq.com>
2023-05-04 21:34:53 +0800

    fix: v18;

--- src/router/comment.router.js
+++ src/router/comment.router.js
@@ -1,15 +1,15 @@
 const KoaRouter = require('@koa/router')
-const commentControoler = require('../controller/comment.controoler')
+const { create, reply, remove } = require('../controller/comment.controoler')
 const { verifyAuth } = require('../middleware/login.middleware')
-const { verifyPermission } = require('../middleware/permission.middleware');
+const { verifyPermission } = require('../middleware/permission.middleware')
 
 const commentRouter = new KoaRouter({ prefix: '/comment' })
 
 // 1.增：发布评论
-commentRouter.post('/', verifyAuth, commentControoler.create)
+commentRouter.post('/', verifyAuth, create)
 // 2.增：回复评论
-commentRouter.post('/reply', verifyAuth, commentControoler.reply)
+commentRouter.post('/reply', verifyAuth, reply)
 // 3.删：删除评论
-commentRouter.delete('/:commentId', verifyAuth, verifyPermission, commentControoler.remove)
+commentRouter.delete('/:commentId', verifyAuth, verifyPermission, remove)
 
 module.exports = commentRouter
