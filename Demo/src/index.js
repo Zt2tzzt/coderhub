10e7243ac4d42cee346b747c05fa885ab0767a4e
ZeT1an <584633819@qq.com>
2023-05-05 23:34:14 +0800

    feat: 08-同源策略-跨域解决方案;

--- Demo/src/index.js
+++ Demo/src/index.js
@@ -1,16 +1,28 @@
 const Koa = require('koa')
 const KoaRouter = require('@koa/router')
-const static = require('koa-static')
+// const static = require('koa-static')
 
 // 创建 Koa 服务器
 const app = new Koa()
 
-app.use(static('./client'))
+// app.use(static('./client'))
 
-app.use(async (ctx, next) => {
+// CORS
+/* app.use(async (ctx, next) => {
   // 1.为简单请求，开启 CORS
   ctx.set('Access-Control-Allow-Origin', '*') // * 表示所有源
-})
+
+  // 2.非简单请求，开启 CORS，还需要下面的设置
+  ctx.set("Access-Control-Allow-Headers", "Accept, AcceptEncoding, Connection, Host, Origin")
+  ctx.set("Access-Control-Allow-Credentials", true) // cookie
+  ctx.set("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS")
+  // 发起的是一个options请求
+  if (ctx.method === 'OPTIONS') {
+    ctx.status = 204
+  } else {
+    await next()
+  }
+}) */
 
 // 创建路由对象
 const userRouter = new KoaRouter({ prefix: '/users' })
@@ -31,4 +43,5 @@ app.use(userRouter.allowedMethods())
 // 开启 Koa 服务器
 app.listen(8000, () => {
   console.log('koa 服务器启动成功~')
+  console.log('haha')
 })
