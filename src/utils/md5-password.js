14dca77bd51ef6fd0d238c31a0c314d2f99d4ea0
ZeT1an <584633819@qq.com>
2023-05-29 09:27:21 +0800

    fix: v42;

--- src/utils/md5-password.js
+++ src/utils/md5-password.js
@@ -1,9 +1,9 @@
 const crypto = require('crypto')
 
-const md5password = password => {
+const md5password = pwd => {
   const md5 = crypto.createHash('md5')
   // 使用 md5 加密，并转成十六进制的形式。
-  const md5pwd = md5.update(password).digest('hex')
+  const md5pwd = md5.update(pwd).digest('hex')
   return md5pwd
 }
 
